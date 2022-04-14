834f0720e9605517f9898d90f7a5325f8bed1980
fabing <1349212501@qq.com>
2022-04-14 11:27:56 +0800

    add throughput latency calculation

--- nas/estimator.py
+++ nas/estimator.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 
 class NonlinearLatencyEstimator:
-    def __init__(self, hardware, model, dummy_input=(1, 3, 224, 224), ops=None):
+    def __init__(self, hardware, model, dummy_input=(1, 3, 224, 224), ops=None, strategy='normal'):
         _logger.info(f'Get latency predictor for applied hardware: {hardware}.')
         self.hardware = hardware
         self.summary = model_summary(model, dummy_input[1:])
@@ -16,6 +16,7 @@ class NonlinearLatencyEstimator:
         self.nonlinear_lat = 0.0
         if ops is None:
             self.ops = ['ReLU', 'MaxPool']
+        self.strategy = strategy
 
         self._get_latency_table()
 
@@ -44,12 +45,36 @@ class NonlinearLatencyEstimator:
         self.linear_lat = linear_lat
         self.nonlinear_lat = non_lat
 
-    def cal_expected_latency(self, current_architecture_prob):
+    def _cal_throughput_latency(self, current_architecture_prob):
+        lat, linear_comm, nonlinear = 0.0, 0.0, 0.0
+        cur_arch = current_architecture_prob.keys()
+        for key in self.block_latency_table.keys():
+            if key in cur_arch:
+                comm = size2memory(self.summary[key]['output_shape']) * self.hardware['communication'] * 2
+                # skip if the identity block has higher probability
+                if torch.argmax(current_architecture_prob[key]) == len(current_architecture_prob[key]) - 1:
+                    continue
+                lat += max(linear_comm + comm,
+                           size2memory(self.summary[key]['output_shape']) * self.hardware['nonlinear'])
+                linear_comm, nonlinear = 0.0, 0.0
+            else:
+                linear_comm += size2memory(self.summary[key]['output_shape']) * self.hardware[
+                    'linear']
+        return lat
+
+    def _cal_normal_latency(self, current_architecture_prob):
         lat = self.linear_lat
         for module_name, probs in current_architecture_prob.items():
             assert len(probs) == len(self.block_latency_table[module_name])
             lat += torch.sum(torch.tensor([probs[i] * self.block_latency_table[module_name][i]
                                            for i in range(len(probs))]))
+        return lat
+
+    def cal_expected_latency(self, current_architecture_prob):
+        if self.strategy == 'normal':
+            lat = self._cal_normal_latency(current_architecture_prob)
+        else:
+            lat = self._cal_throughput_latency(current_architecture_prob)
         return lat
 
     def export_latency(self, current_architecture):
