d6aa07bfb04e566bc01a8117681ffcf605b9f551
Fabing Li <lifabing1349212501@gmail.com>
2022-05-13 17:52:52 +0800

    add multiple module hook and layerchoice latency collection

--- nas/estimator.py
+++ nas/estimator.py
@@ -10,28 +10,47 @@ _logger = logging.getLogger(__name__)
 def _get_module_with_type(root_module, type_name, modules):
     if modules is None:
         modules = []
+    if not isinstance(type_name, (list, tuple)):
+        type_name = [type_name]
 
     def apply(m):
         for name, child in m.named_children():
-            if isinstance(child, type_name):
-                modules.append(child)
-            else:
-                apply(child)
+            for t_name in type_name:
+                if isinstance(child, t_name):
+                    modules.append(child)
+                else:
+                    apply(child)
 
     apply(root_module)
     return modules
 
 
 class NonlinearLatencyEstimator:
-    def __init__(self, hardware, model, dummy_input=(1, 3, 224, 224), target='latency', strategy='latency'):
+    def __init__(self, hardware, model, dummy_input=(1, 3, 224, 224), target='latency'):
         _logger.info(f'Get latency predictor for applied hardware: {hardware}.')
         self.hardware = hardware
-        self.strategy = strategy
         self.in_size = dummy_input
         self.target = target
-        self.block_latency_table = model_latency(model, dummy_input[1:], hardware)
+        self.block_latency_table, self.total_latency = model_latency(model, dummy_input[1:], hardware)
         self.non_ops = _get_module_with_type(model, nn.PReLU, [])
         self.choices = _get_module_with_type(model, nn.LayerChoice, [])
+        self._get_layerchoice_names()
+        self.layerchoice_latency = self._get_layerchoice()
+
+    def _get_layerchoice(self):
+        layerchoice_latency = []
+        idx = 0
+        for name in self.block_latency_table.keys():
+            if name.startswith('LayerChoice'):
+                in_size = [int(i) for i in name.split('_')[1].split('x')]
+                latency = []
+                for choice in self.choices[idx].choices:
+                    _, lat = model_latency(choice, in_size, self.hardware)
+                    latency.append(lat)
+                layerchoice_latency.append(latency)
+                self.total_latency -= sum(latency)
+                idx += 1
+        return layerchoice_latency
 
     def _cal_latency(self):
         lat = .0
