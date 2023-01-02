2eee02a2448cfcd7d077e6976d541abc4452f2ea
Fabing <lifabing1349212501@gmail.com>
2023-01-02 22:04:48 +0800

    add new model

--- models/searchnet.py
+++ models/searchnet.py
@@ -1,9 +1,13 @@
+from collections import OrderedDict
+
 import torch
+import torch.nn as tnn
+import torch.nn.functional as F
 from torch import Tensor
 import nni.retiarii.nn.pytorch as nn
-from typing import Type, Any, Callable, Union, List, Optional
+from typing import Type, Any, Callable, Union, List, Optional, Tuple
 
-from models.sim_ops import ShortcutBlock
+from models.sim_ops import ShortcutBlock, AggregateBlock
 
 
 class ResNet(nn.Module):
@@ -112,6 +116,58 @@ class ResNet(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return self._forward_impl(x)
+
+
+class DenseNet(nn.Module):
+    def __init__(
+        self,
+        num_init_features: int = 64,
+        num_classes: int = 1000,
+    ) -> None:
+
+        super(DenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        self.block1 = AggregateBlock([num_init_features], num_init_features*2, 1)
+        self.block2 = AggregateBlock([num_init_features*2, num_init_features], num_init_features*4, 1)
+        self.block3 = AggregateBlock([num_init_features*4, num_init_features*2, num_init_features],
+                                     num_init_features*8, 1)
+        self.block4 = AggregateBlock([num_init_features*8, num_init_features*4, num_init_features*2, num_init_features],
+                                     num_init_features*16, 1)
+
+
+        # Linear layer
+        self.classifier = nn.Linear(num_init_features * 16, num_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                tnn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                tnn.init.constant_(m.weight, 1)
+                tnn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                tnn.init.constant_(m.bias, 0)
+
+    def forward(self, x: Tensor) -> Tensor:
+        features = [self.features(x)]
+        features.insert(0, self.block1(features))
+        features.insert(0, self.block2(features))
+        features.insert(0, self.block3(features))
+        out = self.block4(features)
+
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
 
 
 def resnet18(pretrained=True, **kwargs: Any) -> ResNet:
--- models/sim_ops.py
+++ models/sim_ops.py
@@ -35,19 +35,20 @@ class ShortcutBlock(nn.Module):
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        compensation = int((kernel_size-3)/2)
+        compensation = int((kernel_size - 3) / 2)
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=kernel_size, stride=stride,
-                               padding=dilation+compensation, groups=groups, bias=False, dilation=dilation)
+                               padding=dilation + compensation, groups=groups, bias=False, dilation=dilation)
         self.bn1 = norm_layer(planes)
         self.sim_relu = conv1x1(planes, planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=kernel_size, stride=1,
-                               padding=1+compensation, groups=1, bias=False, dilation=1)
+                               padding=1 + compensation, groups=1, bias=False, dilation=1)
         self.bn2 = norm_layer(planes)
         if downsample:
             self.downsample = nn.Sequential(
-                    nn.Conv2d(inplanes, planes * self.expansion, kernel_size=kernel_size-2, stride=stride, bias=False, padding=compensation),
-                    norm_layer(planes * self.expansion),
-                )
+                nn.Conv2d(inplanes, planes * self.expansion, kernel_size=kernel_size - 2, stride=stride, bias=False,
+                          padding=compensation),
+                norm_layer(planes * self.expansion),
+            )
         else:
             self.downsample = None
         self.stride = stride
@@ -66,4 +67,52 @@ class ShortcutBlock(nn.Module):
             identity = self.downsample(x)
         out = out + identity
 
+        return out
+
+
+class AggregateBlock(nn.Module):
+    def __init__(
+            self,
+            inplanes: list,
+            outplanes: int,
+            kernel_size: int,
+            stride: int = 1,
+            groups: int = 1,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(AggregateBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        self.layers = []
+        compensation = stride
+        for idx, inp in enumerate(inplanes):
+            self.layers.append(nn.Sequential(
+                nn.Conv2d(inp, 4 * outplanes, kernel_size=kernel_size, stride=compensation,
+                          groups=groups, bias=False),
+                norm_layer(4 * outplanes)
+            ))
+            compensation = 2 ** (idx+1)
+        self.transition = nn.Sequential(
+            nn.Conv2d(4 * outplanes, outplanes, kernel_size=3, stride=1,
+                      padding=1, groups=groups, bias=False),
+            norm_layer(outplanes),
+            nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            norm_layer(outplanes),
+            nn.ReLU(),
+        )
+
+    def forward(self, x: List) -> Tensor:
+    # def forward(self, x: List) -> List:
+        out = None
+        for idx, _ in enumerate(self.layers):
+            if out is None:
+                out = self.layers[idx](x[idx])
+                # out = [self.layers[idx](x[idx])]
+            else:
+                out += self.layers[idx](x[idx])
+                # out.append(self.layers[idx](x[idx]))
+
+        out = self.transition(out)
         return out
