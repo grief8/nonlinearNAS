3505e6b658845b2791956d22bfd88e96189544cf
lifabing <1349212501@qq.com>
2023-01-03 14:33:29 +0800

    add net for cifar

--- models/searchdensenet.py
+++ models/searchdensenet.py
@@ -60,3 +60,52 @@ class DenseNet(nn.Module):
         out = torch.flatten(out, 1)
         out = self.classifier(out)
         return out
+
+
+class CifarDenseNet(nn.Module):
+    def __init__(
+        self,
+        num_init_features: int = 64,
+        num_classes: int = 1000,
+        pretrained: bool = False
+    ) -> None:
+
+        super(CifarDenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+        ]))
+
+        self.block1 = AggregateBlock([num_init_features], num_init_features*2, 1)
+        self.block2 = AggregateBlock([num_init_features*2, num_init_features], num_init_features*4, 1)
+        self.block3 = AggregateBlock([num_init_features*4, num_init_features*2, num_init_features],
+                                     num_init_features*8, 1)
+        # self.block4 = AggregateBlock([num_init_features*8, num_init_features*4, num_init_features*2, num_init_features],
+        #                              num_init_features*16, 1)
+
+        # Linear layer
+        self.classifier = nn.Linear(2048, num_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                tnn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                tnn.init.constant_(m.weight, 1)
+                tnn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                tnn.init.constant_(m.bias, 0)
+
+    def forward(self, x: Tensor) -> Tensor:
+        features = [self.features(x)]
+        features.insert(0, self.block1(features))
+        features.insert(0, self.block2(features))
+        out = self.block3(features)
+
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
