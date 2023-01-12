db5daaf2d42a8aacb19270f640a6e8644d4b6082
Fabing <lifabing1349212501@gmail.com>
2023-01-12 17:31:40 +0800

    add super model implementation

--- models/supermodel.py
+++ models/supermodel.py
@@ -1,7 +1,9 @@
+from collections import OrderedDict
 import torch
 from torch import Tensor
+import torch.nn.functional as F
 import nni.nas.nn.pytorch as nn
-from typing import Type, Any, Callable, Union, List, Optional
+from typing import Tuple, Type, Any, Callable, Union, List, Optional
 from nni.nas.hub.pytorch.nasnet import OPS
 
 from sim_ops import AggregateBlock
@@ -65,7 +67,6 @@ class SampleBlock(torch.nn.ModuleDict):
         self,
         num_layers: int,
         inplanes: int,
-        outplanes: int,
     ) -> None:
         super(SampleBlock, self).__init__()
         for i in range(num_layers):
@@ -79,3 +80,107 @@ class SampleBlock(torch.nn.ModuleDict):
             new_features = layer(sum(features)/len(features))
             features.append(new_features)
         return features[-1]
+
+
+class TransitionBlock(nn.Module):
+    def __init__(
+            self,
+            inplanes: list,
+            outplanes: int,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(TransitionBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        self.layers = nn.ModuleList()
+        compensation = 1
+        config = inplanes[:]
+        config.reverse()
+        for idx, inp in enumerate(config):
+            self.layers.insert(0, nn.Sequential(
+                nn.Conv2d(inp, outplanes, kernel_size=1, stride=compensation, bias=False),
+                norm_layer(outplanes)
+            ))
+            compensation = 2 ** (idx)
+        self.transition = nn.Sequential(
+            nn.Conv2d(outplanes, outplanes, kernel_size=3, stride=2,
+                      padding=1, bias=False),
+            norm_layer(outplanes),
+            nn.ReLU(),
+        )
+
+    def forward(self, x: List) -> Tensor:
+        out = None
+        for idx, _ in enumerate(self.layers):
+            if out is None:
+                out = self.layers[idx](x[idx])
+            else:
+                out += self.layers[idx](x[idx])
+
+        out = self.transition(out)
+        return out
+
+
+class Supermodel(nn.Module):
+    def __init__(
+        self,
+        num_init_features: int = 64,
+        block_config: Tuple[int, int, int, int] = (6, 12, 24, 16),
+        num_classes: int = 1000
+    ) -> None:
+
+        super(Supermodel, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2,
+                                padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        self.samples = nn.ModuleList()
+        self.aggeregate = nn.ModuleList()
+        channels = [num_init_features]
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            # FIXME: Maybe unreasonable without channel adding
+            block = SampleBlock(
+                num_layers=num_layers,
+                inplanes=num_features
+            )
+            self.samples.append(block)
+            if i != len(block_config) - 1:
+                channels.append(num_features)
+                trans = TransitionBlock(inplanes=channels,
+                                    outplanes=num_features * 2)
+                self.aggeregate.append(trans)
+                num_features = num_features * 2
+                channels[-1] = num_features
+        
+        # Linear layer
+        self.classifier = nn.Linear(num_init_features * 16, num_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.nn.init.constant_(m.weight, 1)
+                nn.nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.nn.init.constant_(m.bias, 0)
+
+    def forward(self, x: Tensor) -> Tensor:
+        features = [self.features(x)]
+        for idx in range(len(self.aggeregate)):
+            features.append(self.samples[idx](features[-1]))
+            features[-1] = self.aggeregate[idx](features)
+        out = self.samples[-1](features[-1])
+
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
