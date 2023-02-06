10912781fd9e5c469d117deab3e2af559be9d2d0
Fabing <lifabing1349212501@gmail.com>
2023-02-06 19:08:29 +0800

    modify

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for md in 'searchresnet18' 'searchresnet34' 'searchresnet101' 'searchresnet152';
+for md in 'searchcifarsupermodel16' 'searchcifarsupermodel22' 'searchcifarsupermodel26' 'searchcifarsupermodel50' 'searchcifarsupermodel101';
 do
   # shellcheck disable=SC2034
   for st in 'latency' 'throughput';
@@ -10,7 +10,8 @@ do
       --net "${md}" \
       --dataset "$1" \
       --grad_reg_loss_type "${lt}" \
-      --strategy "${st}"
+      --strategy "${st}" \
+      --choice "v0.7"
     done
   done
 done
