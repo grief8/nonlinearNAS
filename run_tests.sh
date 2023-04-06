4a78414221a90fd3dadf15dd9804f3f797468b69
Fabing LI <lifabing1349212501@gmail.com>
2023-04-06 19:39:15 +0800

    merge conflicts

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 model=$1
 batch=$2
-./run_oneshot_cifar100.sh "${model}" search latency "${batch}" --clamp;
+./run_oneshot_cifar100.sh "${model}" search latency "${batch}" --pretrained;
 ./run_oneshot_cifar100.sh "${model}" retrain latency "${batch}" --clamp;
-./run_oneshot_cifar100.sh "${model}" retrain latency "${batch}" --clamp False;
+./run_oneshot_cifar100.sh "${model}" retrain latency "${batch}" --pretrained;
 ./run_oneshot_cifar100.sh "${model}" retrain latency "${batch}" --std_pruning;
